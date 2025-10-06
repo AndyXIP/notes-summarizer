e4e682dbba9277b07e6827e6afeeb2b45a880a99
AndyXIP <andy.x.p@outlook.com>
2025-10-06 13:07:28 +0100

    update config and database creation

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,9 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+instance_path = os.path.join(basedir, 'instance')
+os.makedirs(instance_path, exist_ok=True)
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -10,5 +13,5 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
-        'sqlite:///' + os.path.join(basedir, 'instance', 'notes-summarizer.sqlite')
+        'sqlite:///' + os.path.join(instance_path, 'notes.sqlite')
     )
